82ef324ec93c0a962c1a78c553086280b00b37e3
Carla Nascimento <carlanascimento321567@gmail.com>
2026-07-20 17:05:11 -0300

    criacao do banco de dados e conexao com as classes

---
+++ batalha_naval/src/ConnectionDataBase.kt
@@ -0,0 +1,21 @@
+import java.sql.Connection
+import java.sql.DriverManager
+
+class ConnectionDataBase {
+    fun criaConexao(): Connection {
+        try {
+            // Avisa o programa para usar o Driver que você adicionou
+            Class.forName("com.mysql.cj.jdbc.Driver")
+
+            // Retorna a conexão configurada com o SEU banco de dados
+            return DriverManager.getConnection(
+                "jdbc:mysql://localhost/batalha_navalbd",
+                "root",
+                "" // senha do seu banco de dados
+            )
+        } catch (e: Exception) {
+            // Se falhar, interrompe o programa mostrando o erro real
+            throw RuntimeException("Erro ao abrir a conexão com o banco de dados", e)
+        }
+    }
+}
---
+++ batalha_naval/src/Partida.kt
@@ -0,0 +1,7 @@
+data class Partida(
+    val idPartida: Int? = null,
+    val jogador: Jogador,
+    val pontuacao: Int,
+    val tempo: Int,
+    val tabuleiro: TipoTabuleiro
+)
