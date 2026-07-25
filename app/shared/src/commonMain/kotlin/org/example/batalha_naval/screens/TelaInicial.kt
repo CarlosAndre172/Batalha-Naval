cbc6f6f2299d002f99fd21638425eaf2c9edbd63
CarlosAndre172 <carlosandrevieira1001@gmail.com>
2026-07-25 14:22:23 -0300

    Update TelaInicial
    Está dando 3 erros por causa dos ícones que estão faltando. É preciso ajustar isso

--- app/shared/src/commonMain/kotlin/org/example/batalha_naval/screens/TelaInicial.kt
+++ app/shared/src/commonMain/kotlin/org/example/batalha_naval/screens/TelaInicial.kt
@@ -2,14 +2,16 @@ package org.example.batalha_naval.screens
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.*
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.material3.*
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.dp
 import org.example.batalha_naval.BotaoAnimado
 
 import androidx.compose.ui.graphics.Color
@@ -26,10 +28,110 @@ fun TelaInicial(
     val AzulOceano = Color(0xFF1A5276)
     val AzulClaro = Color(0xFFD4E6F1)
     val BrancoTexto = Color(0xFFFFFFFF)
-    val MarromMadeira = Color(0xFF8F642D) 
+    val MarromMadeira = Color(0xFF8F642D)
     val Branco = Color(0xFFF5F5DC)
 
-    Surface(
+    var tabuleiroSelecionado by remember { mutableStateOf("") }
+    var powerUpSelecionado by remember { mutableStateOf("") }
+
+    Row {
+
+        // Lado esquerdo
+        Column {
+
+            Text(text = "Rank")
+
+            Text(text = "João")
+            Text(text = "Maria")
+            Text(text = "Pedro")
+            Text(text = "Ana")
+        }
+
+        Spacer(modifier = Modifier.width(24.dp))
+
+        // Lado direito
+        Column {
+
+            // Primeira linha de botões
+            Row {
+
+                listOf("Poça", "Lago", "Mar").forEach { opcaoTabuleiro ->
+
+                    Button(
+                        onClick = { tabuleiroSelecionado = opcaoTabuleiro },
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor =
+                                if (tabuleiroSelecionado == opcaoTabuleiro)
+                                    Color.Blue
+                                else
+                                    Color.Gray
+                        )
+                    ) {
+                        Text(opcaoTabuleiro)
+                    }
+                    Spacer(modifier = Modifier.height(8.dp))
+
+                }
+
+                /*Button(onClick = { }) {
+                Text("Poça")
+                }
+
+                Button(onClick = { }) {
+                    Text("Lago")
+                }
+
+                Button(onClick = { }) {
+                    Text("Mar")
+                }*/
+            }
+
+            // Segunda linha de botões
+            Row {
+
+                /*Button(
+                    onClick = { powerUpSelecionado = opcao },
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor =
+                            if (powerUpSelecionado == opcao)
+                                Color.Blue
+                            else
+                                Color.Gray
+                    )
+                ) {
+                    Text(opcao)
+                }*/
+                Spacer(modifier = Modifier.height(8.dp))
+
+                /*
+                Button(onClick = { }) {
+                    Text("Y")
+                }
+
+                Button(onClick = { }) {
+                    Text("Z")
+                }*/
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                label = {
+                    Text("Nome")
+                }
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(onClick = { }) {
+                Text("START")
+            }
+        }
+    }
+}
+/*    Surface(
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colorScheme.background.copy(alpha = 0.01f)
     ) {
@@ -68,6 +170,78 @@ fun TelaInicial(
                     }
                 }
             }
-        }   
+        }
+    }*/
+
+@Composable
+fun BotoesEscolherTabuleiro() {
+
+    var tabuleiroSelecionado by remember { mutableStateOf("") }
+
+    Box(
+        modifier = Modifier
+            .size(70.dp)
+            .border(
+                3.dp,
+                if (tabuleiroSelecionado == "espada")
+                    Color.Yellow
+                else
+                    Color.Gray
+            )
+            .clickable {
+                tabuleiroSelecionado = "espada"
+            },
+        contentAlignment = Alignment.Center
+    ) {
+        Icon(
+            imageVector = Icons.Default.Home,
+            contentDescription = "Espada"
+        )
+    }
+
+    Spacer(modifier = Modifier.width(10.dp))
+
+    Box(
+        modifier = Modifier
+            .size(70.dp)
+            .border(
+                3.dp,
+                if (tabuleiroSelecionado == "escudo")
+                    Color.Yellow
+                else
+                    Color.Gray
+            )
+            .clickable {
+                tabuleiroSelecionado = "escudo"
+            },
+        contentAlignment = Alignment.Center
+    ) {
+        Icon(
+            imageVector = Icons.Default.Star,
+            contentDescription = "Escudo"
+        )
+    }
+
+    Spacer(modifier = Modifier.width(10.dp))
+
+    Box(
+        modifier = Modifier
+            .size(70.dp)
+            .border(
+                3.dp,
+                if (tabuleiroSelecionado == "bomba")
+                    Color.Yellow
+                else
+                    Color.Gray
+            )
+            .clickable {
+                tabuleiroSelecionado = "bomba"
+            },
+        contentAlignment = Alignment.Center
+    ) {
+        Icon(
+            imageVector = Icons.Default.Face,
+            contentDescription = "Bomba"
+        )
     }
 }
